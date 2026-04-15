701bc8b1b2007149e6e8af34c965d012f52027f3
甄辉 <452210015@qq.com>
2026-04-15 23:47:35 +0800

    面向云代理会话的 VS Code 中的检查点

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
-//创建对象定义属性和方法
-var person = {
-    name: "张三",
-    age: 20,
-    job: "学生",
-    fullname: function() {
-        return this.name + "现在是" + this.age + "岁了";
-    }
+/**
+ * 将页面中id为"demo"的元素的文本颜色设置为红色
+ */
+function highlightText() {
+    document.getElementById("demo").style.color = "red";
 }
-//访问对象属性和方法
-function myFunction() {
-    document.getElementById("demo").innerHTML = person.fullname();
+
+/**
+ * 重置文本颜色为黑色
+ */
+/**
+ * 将演示文本的颜色重置为黑色
+ */
+function resetText() {
+    document.getElementById("demo").style.color = "black";
 }
