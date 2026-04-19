399dda4b045d42dd09114ccaaab83e42aa14d499
甄辉 <452210015@qq.com>
2026-04-19 15:47:04 +0800

    Template string

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
-function MyFunction() {
-    document.getElementById("p1").style.color = "red";
-    document.getElementById("p3").style.color = "red";
-}
-function MyFunction2() {
-    document.getElementById("p1").style.color = "black";
-    document.getElementById("p3").style.color = "black";
-}
+
+// 使用反引号 (``) 定义一个模板字符串，内容为一段 HTML 片段
+let templateString = `<p>这是一个模板字符串示例。'哈哈'</p>`;
+// 通过 id 获取页面中的元素，并将模板字符串作为 HTML 渲染到该元素内部
+document.getElementById("templateString").innerHTML = templateString;
