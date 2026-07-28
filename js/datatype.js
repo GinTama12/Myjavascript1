edaa3f3430db49f222c66d6743dd4f9492089028
GinTama12 <452210015@qq.com>
2026-07-29 00:31:42 +0800

    typeof
    Signed-off-by: GinTama12 <452210015@qq.com>

--- js/datatype.js
+++ js/datatype.js
@@ -24,3 +24,18 @@ document.write(message + "<br>");
 let emaile = prompt("请输入邮箱地址");
 document.write(`您输入的邮箱地址是；${emaile}<br>`);
 document.write(`您输入的邮箱地址是；${emaile}<br>`);
+let ageInput = prompt("请输入您的年龄");
+if (ageInput >= 18) {
+  document.write(`你已经成年了，可以进入网站<br>`);
+} else {
+  document.write(`你还没成年，不能进入网站<br>`);
+}
+let num1;
+document.write(`num1的值是：${num1}<br>`);
+let num2 = null;
+document.write(`num2的值是：${num2 + 1}<br>`);
+//检查数据类型
+document.write(`num1的数据类型是：${typeof num1}<br>`);
+document.write(`num2的数据类型是：${typeof num2}<br>`);
+document.write(`message的数据类型是：${typeof message}<br>`);
+document.write(`arr的数据类型是：${typeof arr}<br>`);
